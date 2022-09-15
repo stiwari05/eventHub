ff378ddc2d1695283105904f868cddc911c4c338
Sanjay <sanjay_tiwari_@hotmail.com>
2022-09-15 10:25:43 -0500

    first commit

---
+++ 01-SendEvents/Program.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_SendEvents
+{
+    class Program
+    {
+        private const string connectionString = "Endpoint=sb://stdemo.servicebus.windows.net/;SharedAccessKeyName=my-sender;SharedAccessKey=KsZT7IfHfYj6bMWBoijb6MYW480CsJ9SgUKIM5kzA/o=;EntityPath=my-demo";
+        private const string eventHubName = "my-demo";
+
+        static async Task Main()
+        {
+            await using (var producerClient = new EventHubProducerClient(connectionString, eventHubName))
+            {
+                using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("First Event")));
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Second Event")));
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Third Event")));
+
+                await producerClient.SendAsync(eventBatch);
+
+                Console.WriteLine("A batch of 3 events have been published.");
+
+            }
+        }
+    }
+}
---
+++ 02-RecieveEvents/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+
+namespace _02_RecieveEvents
+{
+    class Program
+    {
+        private const string connectionString = "Endpoint=sb://stdemo.servicebus.windows.net/;SharedAccessKeyName=my-reciever;SharedAccessKey=ob00/mRMsyZugp3VJrYF/EYSY6RaWjep8ls7CC8wALI=;EntityPath=my-demo";
+        private const string eventHubName = "my-demo";
+        private const string blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=azeventhubdemost;AccountKey=kt7mk95Oy8hj7XIRtpiwCRrK9sMteImD42QuVH33NR7QkqFb9vLLSaI1lH4xP1cwgWgEM+IA6+KN+ASt14D3pg==;EndpointSuffix=core.windows.net";
+        private const string blobContainerName = "checkpoints";
+
+        static async Task Main()
+        {
+            string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+
+            BlobContainerClient storageClient = new BlobContainerClient(blobStorageConnectionString, blobContainerName);
+            EventProcessorClient processor = new EventProcessorClient(storageClient, consumerGroup, connectionString, eventHubName);
+
+            processor.ProcessEventAsync += ProcessEventHandler;
+            processor.ProcessErrorAsync += ProcessErrorHandler;
+
+            await processor.StartProcessingAsync();
+            await Task.Delay(TimeSpan.FromSeconds(10));
+
+            await processor.StopProcessingAsync();
+        }
+
+        static async Task ProcessEventHandler(ProcessEventArgs eventArgs)
+        {
+            // Write the body of the event to the console window
+            Console.WriteLine("\tRecevied event: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
+
+            // Update checkpoint in the blob storage so that the app receives only new events the next time it's run
+            await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
+        }
+
+        static Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+        {
+            // Write details about the error to the console window
+            Console.WriteLine($"\tPartition '{ eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+            Console.WriteLine(eventArgs.Exception.Message);
+            return Task.CompletedTask;
+        }
+
+    }
+}
